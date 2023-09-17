fd8b7076d5dc73961eaef5928e494907e39c8997
DarshanBhokare <56796768+DarshanBhokare@users.noreply.github.com>
2023-09-17 10:53:27 -0400

    Error Handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 
 const router = express.Router();
@@ -9,11 +11,15 @@ router.post('/api/users/signup', [
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be within 4 and 20 characters')
 ],
 (req: Request, res: Response) => {
-    const {email, password} =req.body;
-
-    if(!email || typeof email!== 'string'){
-        res.status(400).send('Provide a valid email');
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        throw new RequestValidationError((errors.array()));
     }
+
+    console.log('Creating a user');
+    throw new DatabaseConnectionError();
+    res.send({});
+  
 });
 
 export { router as signupRouter};
