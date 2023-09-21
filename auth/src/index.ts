3a8a142054f15d2898e79cfd6edc2f375ddaf5b8
DarshanBhokare <bhokare.d@northeastern.edu>
2023-09-21 11:34:23 -0400

    Worked on error handling,database modelling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
@@ -23,6 +23,16 @@ app.all('/*', async(req,res) =>{
 });
 app.use(errorHandler);
 
+const start =async () => {
+  try{
+  await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  console.log('Connected to MongoDB');
+} catch(err){
+  console.error(err);
+}
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
+  console.log('Listening on port 3000');
 });
+};
+start();
+
