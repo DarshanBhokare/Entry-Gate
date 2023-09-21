6ff5bed47f2e32a3504e03a7659ac555b03bc3df
DarshanBhokare <bhokare.d@northeastern.edu>
2023-09-21 19:59:19 -0400

    Implemented signup, signin, currentuser flow

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,11 @@
 import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-requests';
 
 
 const router = express.Router();
@@ -11,12 +14,8 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be within 4 and 20 characters')
 ],
+validateRequest,
 async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        throw new RequestValidationError((errors.array()));
-    }
-
     const {email,password}=req.body;
     
     const existingUser = await User.findOne({email});
@@ -29,6 +28,17 @@ async (req: Request, res: Response) => {
     const user = User.build({email,password});
     await user.save();
 
+    // Generate JWT
+    const userJWT = jwt.sign({
+        id:user.id,
+        email: user.email
+    }, process.env.JWT_KEY!
+    );
+
+    // Store JWT on session object
+    req.session ={
+        jwt: userJWT
+    };
     res.status(201).send(user);
 });
 
